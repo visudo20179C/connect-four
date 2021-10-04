a2359cb6fe440f74162667bd78ca92b66acc61cd
visudo <visudo20179C@protonmail.com>
2021-10-03 22:12:19 -0400

    fix nav bar wrapping

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,8 +1,8 @@
 <template>
-	<nav class="border-b-2 border-yellow-300 flex itemster justify-between flex-wrap bg-blue-900 p-6">
-		<div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+	<nav class="border-b-2 border-yellow-300 flex itemster justify-between flex-row bg-blue-900 p-6">
+		<div class="w-full">
 			<div class="text-sm lg:flex-grow">
-				<div class="float-right mt-auto mb-auto">
+				<div class="flex flex-row float-right mt-auto mb-auto">
 					<a href="https://github.com/visudo20179C/connect-four" target="_blank" class="block ml-96 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4 text-lg font-black">
 						GitHub
 					</a>
